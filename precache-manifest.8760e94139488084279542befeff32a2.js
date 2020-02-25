5e40d762c0b61b5b66ab3f6c287d5d7922bbde9f
Sandra Rosa <sandra.luiza.rosa@gmail.com>
2020-02-25 19:22:21 +0100

    Updates

---
+++ precache-manifest.8760e94139488084279542befeff32a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c9d6a28d999d2a808e5",
+    "url": "/weatherApp/static/css/main.77dfff79.chunk.css"
+  },
+  {
+    "revision": "7c9d6a28d999d2a808e5",
+    "url": "/weatherApp/static/js/main.625c41fd.chunk.js"
+  },
+  {
+    "revision": "b0ae699ea3cc63fef9c8",
+    "url": "/weatherApp/static/js/runtime~main.25533534.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/weatherApp/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "8b5b3ba96cd1f482153b59846ee5e307",
+    "url": "/weatherApp/static/media/bg-app.8b5b3ba9.jpg"
+  },
+  {
+    "revision": "626d80c0603f9774a9b11eb9efb60929",
+    "url": "/weatherApp/index.html"
+  }
+];
